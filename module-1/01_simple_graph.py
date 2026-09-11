f5159efc2d26dbed7a3b88cfb75da2aede93d3f4
Vishal Sindham <vishalsindhamwork@gmail.com>
2026-09-11 16:41:46 +0530

    Simple graph which explores the basic components of the LangGraph which are Nodes , Edges and Conditional Edges

--- module-1/01_simple_graph.py
+++ module-1/01_simple_graph.py
@@ -1,32 +1,38 @@
-from typing_extensions import TypedDict
 import random
 from typing import Literal
+
 from IPython.display import Image, display
-from langgraph.graph import StateGraph, START, END
+from langgraph.graph import END, START, StateGraph
+from typing_extensions import TypedDict
+
 
 class State(TypedDict):
     graph_state: str
 
+
 def node_1(state):
     print("--- Node 1 ---")
-    return {"graph_state" : state['graph_state'] + " I am"}
+    return {"graph_state": state["graph_state"] + " I am"}
+
 
 def node_2(state):
     print("--- Node 2 ---")
-    return {"graph_state" : state['graph_state'] + " happy!"}
+    return {"graph_state": state["graph_state"] + " happy!"}
+
 
 def node_3(state):
     print("--- Node 3 ---")
-    return {"graph_state" : state['graph_state'] + " content!"}
+    return {"graph_state": state["graph_state"] + " content!"}
+
 
 def decide_mood(state) -> Literal["second", "third"]:
 
-    user_input = state['graph_state']
+    user_input = state["graph_state"]
 
     if random.random() < 0.5:
 
         return "second"
-    
+
     return "third"
 
 
@@ -44,6 +50,6 @@ graph = builder.compile()
 
 graph.get_graph().draw_mermaid_png(output_file_path="simple_graph.png")
 
-response = graph.invoke({"graph_state" : "Hi, this is Vishal. "})
+response = graph.invoke({"graph_state": "Hi, this is Vishal. "})
 
 print(response)
