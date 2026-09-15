1c6406f55218e3953c2b05b88215259bdf5f582e
Vishal Sindham <vishalsindhamwork@gmail.com>
2026-09-15 10:30:16 +0530

    Basic router pattern to understand the router and routing in LangChain ecosystem and LLM applications.

---
+++ module-1/router.py
@@ -0,0 +1,51 @@
+from dotenv import load_dotenv
+from IPython.display import Image, display
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph import MessagesState
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage
+
+
+load_dotenv()
+
+def multiply(a: int, b: int) -> int:
+    """Multiply a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash-lite")
+llm_with_tools = llm.bind_tools([multiply])
+
+# Node
+def tool_calling_llm(state: MessagesState):
+    return {"messages" : [llm_with_tools.invoke(state["messages"])]}
+
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("tools_llm", tool_calling_llm)
+builder.add_node("tools", ToolNode([multiply]))
+builder.add_edge(START, "tools_llm")
+builder.add_conditional_edges(
+    "tools_llm",
+    tools_condition
+)
+
+builder.add_edge("tools",END)
+
+# Graph
+graph = builder.compile()
+
+# Image(graph.get_graph().draw_mermaid_png(output_file_path="router.png"))
+
+
+msgs = [HumanMessage(content="Hello, What is 5 multiplied by 6.?")]
+msgs = graph.invoke({"messages": msgs})
+
+for msg in msgs['messages']:
+    msg.pretty_print()
+
