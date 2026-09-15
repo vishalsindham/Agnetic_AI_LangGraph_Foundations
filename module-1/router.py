4e4d801dee6b0ee398d0c35b53a973afbaeafcc8
Vishal Sindham <vishalsindhamwork@gmail.com>
2026-09-15 10:31:00 +0530

    Basic router pattern to understand the router and routing in LangChain ecosystem and LLM applications.

--- module-1/router.py
+++ module-1/router.py
@@ -1,13 +1,12 @@
 from dotenv import load_dotenv
 from IPython.display import Image, display
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph import MessagesState
-from langgraph.prebuilt import ToolNode, tools_condition
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage
-
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph import END, START, MessagesState, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
 
 load_dotenv()
+
 
 def multiply(a: int, b: int) -> int:
     """Multiply a and b.
@@ -18,24 +17,24 @@ def multiply(a: int, b: int) -> int:
     """
     return a * b
 
+
 llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash-lite")
 llm_with_tools = llm.bind_tools([multiply])
 
+
 # Node
 def tool_calling_llm(state: MessagesState):
-    return {"messages" : [llm_with_tools.invoke(state["messages"])]}
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
 
 # Build graph
 builder = StateGraph(MessagesState)
 builder.add_node("tools_llm", tool_calling_llm)
 builder.add_node("tools", ToolNode([multiply]))
 builder.add_edge(START, "tools_llm")
-builder.add_conditional_edges(
-    "tools_llm",
-    tools_condition
-)
+builder.add_conditional_edges("tools_llm", tools_condition)
 
-builder.add_edge("tools",END)
+builder.add_edge("tools", END)
 
 # Graph
 graph = builder.compile()
@@ -46,6 +45,5 @@ graph = builder.compile()
 msgs = [HumanMessage(content="Hello, What is 5 multiplied by 6.?")]
 msgs = graph.invoke({"messages": msgs})
 
-for msg in msgs['messages']:
+for msg in msgs["messages"]:
     msg.pretty_print()
-
